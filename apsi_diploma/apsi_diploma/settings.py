15c69c2896476b56c57edb730cd4214a9ba21e79
Michał Łątkowski <michallatkos@gmail.com>
2022-05-23 17:32:46 +0200

    Make settings dependent on production and development instances

--- apsi_diploma/apsi_diploma/settings.py
+++ apsi_diploma/apsi_diploma/settings.py
@@ -17,18 +17,19 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-wv8nxxyjd)nvpc8^n#!ahui0nw+$kcbhi6!@bomr=g)t(2uoj6"
+SECRET_KEY = os.environ.get("DJANGO_APSI_DIPLOMA_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+if os.environ.get("IS_HEROKU_PRODUCTION"):
+    DEBUG = False
+else:
+    DEBUG = True
 
 ALLOWED_HOSTS = ["apsi-dyplomowanie.herokuapp.com"]
-
 
 # Application definition
 
@@ -73,7 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "apsi_diploma.wsgi.application"
 
-
 DATABASES = {
     "default": {
         "ENGINE": "djongo",
@@ -102,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -113,7 +112,6 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
