451df476a1d3ac9bc1eeabf3e6fae96f58606932
Latkos <48084189+Latkos@users.noreply.github.com>
2022-06-26 14:12:10 +0200

    Changing STATICFILES_STORAGE

--- apsi_diploma/apsi_diploma/settings.py
+++ apsi_diploma/apsi_diploma/settings.py
@@ -159,7 +159,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "diploma_app/static/"),)
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
